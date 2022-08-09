e0b99c5a207d37cfff1bbedc059e59a2354e9c3b
heyjochen <jochen@stierberger.com>
2022-08-09 17:56:07 -0500

    import errorhandler, change route paths

--- server/index.js
+++ server/index.js
@@ -1,15 +1,33 @@
+const path = require('path');
 const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv').config();
+require('dotenv').config();
 const port = process.env.PORT || 5000;
+const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./db/config');
-const app = express();
 
+const app = express();
 connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+
+app.use('/api/projects', require('./routes/projectsRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/newsletter', require('./routes/newsletterRoute'));
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'client', 'build', 'index.html')
+    )
+  );
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'));
+}
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`We runnin on port ${port}`));
