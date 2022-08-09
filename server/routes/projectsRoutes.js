aa3b42ed481241f34dbb53c11d07fc2b49f9061d
heyjochen <jochen@stierberger.com>
2022-08-09 17:54:27 -0500

    add routes

---
+++ server/routes/projectsRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+  getProjects,
+  setProject,
+  updateProject,
+  deleteProject,
+} = require('../controllers/projectsController.js');
+
+const { protect } = require('../middleware/authMiddleware');
+
+router.route('/').get(protect, getProjects).post(protect, setProject);
+router.route('/:id').delete(protect, deleteProject).put(protect, updateProject);
+
+module.exports = router;
