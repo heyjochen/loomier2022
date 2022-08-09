2240d9be86fa122e1f3fcd6fcc8f63d532aef208
heyjochen <jochen@stierberger.com>
2022-08-09 13:07:13 -0500

    init BrowserRouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Dashboard } from './pages/Dashboard';
+import { Projects } from './components/Projects';
+import { Create } from './components/Create';
+import { Profile } from './components/Profile';
+
 function App() {
-  return <h1 className="bg-slate-400">jsdlf</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="dashboard" element={<Dashboard />}>
+        {/* <Route path="profile" element={<Profile />} /> */}
+        <Route path="projects" element={<Projects />} />
+        <Route path="create" element={<Create />} />
+        <Route path="profile" element={<Profile />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
