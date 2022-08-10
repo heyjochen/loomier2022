d82d3b2ec4c5fe33526b2f52fe27e75ffbd58d73
heyjochen <jochen@stierberger.com>
2022-08-10 13:37:19 -0500

    define crud operations

--- server/controllers/projectsController.js
+++ server/controllers/projectsController.js
@@ -1,55 +1,44 @@
-const asyncHandler = require('express-async-handler');
+const Project = require('../models/projectSchema');
 
-const Project = require('../models/projectModel');
-const User = require('../models/userModel');
+// @desc Get all projects
+// @GET /api/projects
+// access PRIVATE
+const getProjects = async (req, res) => {
+  const projects = await Project.find({});
+  res.status(200).send(projects);
+};
 
-// @desc    Get projects
-// @route   GET /api/projects
-// @access  Private
-const getProjects = asyncHandler(async (req, res) => {
-  const projects = await project.find({ user: req.user.id });
+// @desc Create a project
+// @POST /api/projects
+// access PRIVATE
+const setProject = async (req, res) => {
+  const { title, camera, lens, film, notes } = req.body;
 
-  res.status(200).json(projects);
-});
-
-// @desc    Set project
-// @route   POST /api/projects
-// @access  Private
-const setProject = asyncHandler(async (req, res) => {
-  if (!req.body.text) {
+  if (!title) {
     res.status(400);
-    throw new Error('Please add a text field');
+    throw new Error('Please add a title');
   }
 
   const project = await Project.create({
-    text: req.body.text,
-    user: req.user.id,
+    title,
+    camera,
+    lens,
+    film,
+    notes,
   });
 
   res.status(200).json(project);
-});
+};
 
-// @desc    Update project
-// @route   PUT /api/projects/:id
-// @access  Private
-const updateProject = asyncHandler(async (req, res) => {
+// @desc Update a project
+// @PUT /api/projects/:id
+// access PRIVATE
+const updateProject = async (req, res) => {
   const project = await Project.findById(req.params.id);
 
   if (!project) {
     res.status(400);
     throw new Error('project not found');
-  }
-
-  // Check for user
-  if (!req.user) {
-    res.status(401);
-    throw new Error('User not found');
-  }
-
-  // Make sure the logged in user matches the project user
-  if (project.user.toString() !== req.user.id) {
-    res.status(401);
-    throw new Error('User not authorized');
   }
 
   const updatedProject = await Project.findByIdAndUpdate(
@@ -61,12 +50,12 @@ const updateProject = asyncHandler(async (req, res) => {
   );
 
   res.status(200).json(updatedProject);
-});
+};
 
-// @desc    Delete project
-// @route   DELETE /api/projects/:id
-// @access  Private
-const deleteProject = asyncHandler(async (req, res) => {
+// @desc Delete a project
+// @route /api/projects/:id
+// access PRIVATE
+const deleteProject = async (req, res) => {
   const project = await Project.findById(req.params.id);
 
   if (!project) {
@@ -74,26 +63,9 @@ const deleteProject = asyncHandler(async (req, res) => {
     throw new Error('project not found');
   }
 
-  // Check for user
-  if (!req.user) {
-    res.status(401);
-    throw new Error('User not found');
-  }
-
-  // Make sure the logged in user matches the project user
-  if (project.user.toString() !== req.user.id) {
-    res.status(401);
-    throw new Error('User not authorized');
-  }
-
   await project.remove();
 
   res.status(200).json({ id: req.params.id });
-});
-
-module.exports = {
-  getProjects,
-  setProject,
-  updateProject,
-  deleteProject,
 };
+
+module.exports = { getProjects, setProject, updateProject, deleteProject };
