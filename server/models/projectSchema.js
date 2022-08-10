1ba3ec713a47c57dd85c97582e16ae9b9a968bd6
heyjochen <jochen@stierberger.com>
2022-08-10 13:52:14 -0500

    create DB model for Projects

---
+++ server/models/projectSchema.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const projectSchema = mongoose.Schema(
+  {
+    title: String,
+    camera: String,
+    lens: String,
+    film: String,
+    notes: String,
+    creator: String,
+    // createdAt: {
+    //   type: Date,
+    //   default: new Date(),
+    // },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Project', projectSchema);
