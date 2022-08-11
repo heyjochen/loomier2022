1c6bb2f7b6db476100cd99649de87503387e8cd5
heyjochen <jochen@stierberger.com>
2022-08-11 18:32:53 -0500

    implement state to track nav active link

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -2,51 +2,61 @@ import { Fragment, useState } from 'react';
 import { Dialog, Menu, Transition } from '@headlessui/react';
 import {
   BellIcon,
-  CalendarIcon,
   FolderIcon,
   HomeIcon,
-  InboxIcon,
   MenuAlt2Icon,
   UsersIcon,
   XIcon,
+  PlusIcon,
 } from '@heroicons/react/outline';
 import { SearchIcon } from '@heroicons/react/solid';
 import { Link, Outlet } from 'react-router-dom';
 
-const navigation = [
-  { name: 'Dashboard', href: '/dashboard', icon: HomeIcon, current: true },
-  {
-    name: 'Profile',
-    href: '/dashboard/profile',
-    icon: UsersIcon,
-    current: false,
-  },
-  {
-    name: 'Projects',
-    href: '/dashboard/projects',
-    icon: FolderIcon,
-    current: false,
-  },
-  { name: 'Calendar', href: '#', icon: CalendarIcon, current: false },
-  {
-    name: 'Create',
-    href: '/dashboard/create#',
-    icon: InboxIcon,
-    current: false,
-  },
-];
-const userNavigation = [
-  { name: 'Your Profile', href: '#' },
-  { name: 'Settings', href: '#' },
-  { name: 'Sign out', href: '#' },
-];
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ');
-}
-
 export function Dashboard() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [active, setActive] = useState('Dashboard');
+
+  const navigation = [
+    {
+      name: 'Dashboard',
+      href: '/dashboard/dash',
+      icon: HomeIcon,
+      current: true,
+    },
+    {
+      name: 'Create',
+      href: '/dashboard/create',
+      icon: PlusIcon,
+      current: false,
+    },
+    {
+      name: 'Profile',
+      href: '/dashboard/profile',
+      icon: UsersIcon,
+      current: false,
+    },
+    {
+      name: 'Projects',
+      href: '/dashboard/projects',
+      icon: FolderIcon,
+      current: false,
+    },
+    {
+      name: 'Exposures',
+      href: '/dashboard/exposures',
+      icon: FolderIcon,
+      current: false,
+    },
+  ];
+  const userNavigation = [
+    { name: 'Your Profile', href: '#' },
+    { name: 'Settings', href: '#' },
+    { name: 'Sign out', href: '#' },
+  ];
+
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(' ');
+  }
 
   return (
     <>
@@ -122,18 +132,20 @@ export function Dashboard() {
                     <nav className="px-2 space-y-1">
                       {navigation.map((item) => (
                         <Link
+                          onClick={() => setActive(item.name)}
                           key={item.name}
                           to={item.href}
                           className={classNames(
-                            item.current
+                            active === item.name
                               ? 'bg-gray-100 text-gray-900'
                               : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900',
                             'group flex items-center px-2 py-2 text-base font-medium rounded-md'
                           )}
                         >
                           <item.icon
+                            onClick={() => setActive(item.name)}
                             className={classNames(
-                              item.current
+                              active === item.name
                                 ? 'text-gray-500'
                                 : 'text-gray-400 group-hover:text-gray-500',
                               'mr-4 flex-shrink-0 h-6 w-6'
@@ -169,18 +181,20 @@ export function Dashboard() {
               <nav className="flex-1 px-2 pb-4 space-y-1">
                 {navigation.map((item) => (
                   <Link
+                    onClick={() => setActive(item.name)}
                     key={item.name}
                     to={item.href}
                     className={classNames(
-                      item.current
+                      active === item.name
                         ? 'bg-gray-100 text-gray-900'
                         : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900',
                       'group flex items-center px-2 py-2 text-sm font-medium rounded-md'
                     )}
                   >
                     <item.icon
+                      onClick={() => setActive(item.name)}
                       className={classNames(
-                        item.current
+                        active === item.name
                           ? 'text-gray-500'
                           : 'text-gray-400 group-hover:text-gray-500',
                         'mr-3 flex-shrink-0 h-6 w-6'
@@ -278,7 +292,7 @@ export function Dashboard() {
           </div>
 
           <main className="flex-1">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+            <div className="mx-auto">
               {/* Replace with your content */}
               <Outlet />
               {/* /End replace */}
