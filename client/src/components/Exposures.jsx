86700b5a3696203e3ee78ed4a837bb54a0455641
heyjochen <jochen@stierberger.com>
2022-08-11 13:47:28 -0500

    remove log, create Delete button

--- client/src/components/Exposures.jsx
+++ client/src/components/Exposures.jsx
@@ -53,7 +53,7 @@ function classNames(...classes) {
 
 export function Exposures() {
   const [exposures, setExposures] = useState([]);
-  console.log(exposures);
+
   useEffect(() => {
     const allExposures = async () => {
       try {
@@ -147,6 +147,10 @@ export function Exposures() {
                   className="pr-6 py-3 border-b border-gray-200 bg-gray-50 text-right text-xs font-medium text-gray-500 uppercase tracking-wider"
                   scope="col"
                 />
+                <th
+                  className="pr-6 py-3 border-b border-gray-200 bg-gray-50 text-right text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  scope="col"
+                />
               </tr>
             </thead>
             <tbody className="bg-white divide-y divide-gray-100">
@@ -195,6 +199,14 @@ export function Exposures() {
                       className="text-indigo-600 hover:text-indigo-900"
                     >
                       Edit
+                    </a>
+                  </td>
+                  <td className="px-6 py-3 whitespace-nowrap text-right text-sm font-medium">
+                    <a
+                      href="#"
+                      className="text-indigo-600 hover:text-indigo-900"
+                    >
+                      Delete
                     </a>
                   </td>
                 </tr>
