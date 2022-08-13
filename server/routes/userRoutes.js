e9b6fbe44dec5675bcb1dec025d2987e5acc192a
heyjochen <jochen@stierberger.com>
2022-08-13 13:54:19 -0500

    start user auth

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,10 +5,9 @@ const {
   loginUser,
   getMe,
 } = require('../controllers/userController');
-const { protect } = require('../middleware/authMiddleware');
 
 router.post('/', registerUser);
 router.post('/login', loginUser);
-router.get('/me', protect, getMe);
+router.post('/me', getMe);
 
 module.exports = router;
