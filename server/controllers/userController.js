2a400c358d60956f9e1a071cd2db19f0d7a7018c
heyjochen <jochen@stierberger.com>
2022-08-14 14:10:44 -0500

    handle user auth

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,21 +1,77 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const User = require('../models/userSchema');
+const asyncHandler = require('express-async-handler');
+
 // @desc Get all projects
 // @POST /api/users
 // access PRIVATE
-const registerUser = (req, res) => {
-  res.send('Register user');
-};
+const registerUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  console.log(email, password);
+
+  if (!email || !password) {
+    res.status(400);
+    throw new Error('Please add all fields');
+  }
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPW = await bcrypt.hash(password, salt);
+
+  const user = await User.create({
+    email,
+    password: hashedPW,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+});
 
 // @desc Auth a user
 // @POST /api/users/login
 // access PUBLIC
-const loginUser = (req, res) => {
-  res.json({ message: 'Auth User' });
-};
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid credentials');
+  }
+});
 
 // @desc Register new user
 // @POST /api/users
-// access PUBLIC
-const getMe = (req, res) => {
+// access PRIVATE
+const getMe = asyncHandler(async (req, res) => {
   res.send('register user');
+});
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
 };
+
 module.exports = { registerUser, loginUser, getMe };
